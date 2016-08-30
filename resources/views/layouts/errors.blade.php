a29d704be59684844e6ebc13712160dfa3cd21b1
Victor <vikie95@gmail.com>
2016-08-30 13:33:49 +0100

    error danger class

--- resources/views/layouts/errors.blade.php
+++ resources/views/layouts/errors.blade.php
@@ -1,7 +1,7 @@
 @if (count($errors) > 0)
     <ul>
         @foreach ($errors->all() as $error)
-            <div class="alert alert-info">
+            <div class="alert alert-danger">
                 <span>{{$error}}</span>
             </div>
         @endforeach
